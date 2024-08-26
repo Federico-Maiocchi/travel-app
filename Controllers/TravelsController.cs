5cd6e67fa81537b3b501b1c9a84189349afe5b47
Federico Maiocchi <maiocchifederico@gmail.com>
2024-08-26 11:45:40 +0200

    FIX: Create in TravelsController

---
+++ Controllers/TravelsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using travel_app.Data;
+using travel_app.Models;
+
+namespace travel_app.Controllers
+{
+    public class TravelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TravelsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Travels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.travels.ToListAsync());
+        }
+
+        // GET: Travels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travel = await _context.travels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (travel == null)
+            {
+                return NotFound();
+            }
+
+            return View(travel);
+        }
+
+        // GET: Travels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Travels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Travel travel)
+        {
+            travel.CretedById = "Admin";
+            travel.CretedOn = DateTime.Now;
+
+
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Add(travel);
+            //    await _context.SaveChangesAsync();
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //return View(travel);
+
+            _context.Add(travel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Travels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travel = await _context.travels.FindAsync(id);
+            if (travel == null)
+            {
+                return NotFound();
+            }
+            return View(travel);
+        }
+
+        // POST: Travels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StartDate,EndDate,CretedById,CretedOn,ModifiedById,ModifiedOn")] Travel travel)
+        {
+            if (id != travel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(travel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TravelExists(travel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(travel);
+        }
+
+        // GET: Travels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var travel = await _context.travels
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (travel == null)
+            {
+                return NotFound();
+            }
+
+            return View(travel);
+        }
+
+        // POST: Travels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var travel = await _context.travels.FindAsync(id);
+            if (travel != null)
+            {
+                _context.travels.Remove(travel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TravelExists(int id)
+        {
+            return _context.travels.Any(e => e.Id == id);
+        }
+    }
+}

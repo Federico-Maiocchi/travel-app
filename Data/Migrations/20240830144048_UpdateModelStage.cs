7930cdd824b7bb0cc98c797bdbd58e4bd9f55699
Federico Maiocchi <maiocchifederico@gmail.com>
2024-08-30 16:47:09 +0200

    FIX: property Model/Stage and Add validations

---
+++ Data/Migrations/20240830144048_UpdateModelStage.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace travel_app.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelStage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Vote",
+                table: "stages",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "stages",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "stages",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "stages",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "stages",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Vote",
+                table: "stages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "stages",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "stages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "stages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "stages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
--- Models/Stage.cs
+++ Models/Stage.cs
@@ -4,28 +4,36 @@ namespace travel_app.Models
 {
     public class Stage: UserActivity
     {
+       
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Il campo 'Title' è obbligatorio.")]
+        [StringLength(100, ErrorMessage = "Il campo 'Title' può contenere al massimo 100 caratteri.")]
         public string Title { get; set; }
 
-        public string Description { get; set; }
+        [StringLength(500, ErrorMessage = "Il campo 'Description' può contenere al massimo 500 caratteri.")]
+        public string? Description { get; set; }
 
-        public string Image{ get; set; }
+        public string? Image{ get; set; }
 
-        public string Note { get; set; }
+        [StringLength(300, ErrorMessage = "Il campo 'Note' può contenere al massimo 300 caratteri.")]
+        public string? Note { get; set; }
 
-        public int Vote { get; set; }
+        [Range(1, 5, ErrorMessage = "Il valore del campo 'Voto' deve essere compreso tra 1 e 5.")]
+        public int? Vote { get; set; }
 
+        [Required(ErrorMessage = "Il campo 'Data' è obbligatorio.")]
         public DateTime Date { get; set; }
 
         public double Latitude { get; set; }
 
         public double Longitude { get; set; }
 
+        [Required(ErrorMessage = "Il campo 'TravelId' è obbligatorio.")]
         public int TravelId { get; set; }
 
         public Travel Travel { get; set; }
 
-        public bool Completed { get; set; }
+        public bool Completed { get; set; } = false;
     }
 }
